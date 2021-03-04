f3d6ddb5647e8273939d4edb9b8bd9056e42057c
bhimasty <48201353+bhimasty@users.noreply.github.com>
2021-03-04 13:54:37 +0700

    Add files via upload

---
+++ pertemuan19/login.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+require "functions.php";
+
+// cek cookie
+if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
+  $id = $_COOKIE['id'];
+  $key = $_COOKIE['key'];
+
+  // ambil username berdasarkan id
+  $result = mysqli_query(koneksi(), "SELECT username FROM user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+
+  // cek cookie dan username
+  if ($key === hash('sha256', $row['username'])) {
+    $_SESSION['login'] = true;
+  }
+}
+
+if (isset($_SESSION['login'])) {
+  header("Location: index.php");
+  exit;
+}
+
+if (isset($_POST['login'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $result = mysqli_query(koneksi(), "SELECT * FROM user WHERE username = '$username'");
+
+  if (mysqli_num_rows($result) === 1) {
+    // cek password
+    $row = mysqli_fetch_assoc($result);
+    if (password_verify($password, $row['password'])) {
+
+      // set session
+      $_SESSION['login'] = true;
+
+      // cek remember me
+      if (isset($_POST['remember'])) {
+        // buat cookie
+        setcookie('id', $row['id'], time() + 60);
+        setcookie('key', hash('sha256', $row['username']), time() + 60);
+      }
+
+      header("Location: index.php");
+      exit;
+    }
+  }
+
+  $error = true;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+</head>
+
+<body>
+  <a href="registrasi.php">Daftar</a>
+
+  <h1>Halaman Login</h1>
+
+  <?php if (isset($error)) : ?>
+    <p style="color: red; font-style: italic">Username atau Password Salah</p>
+  <?php endif; ?>
+
+  <form action="" method="post">
+    <ul type="none">
+      <li>
+        <label>
+          Username :
+          <input type="text" name="username">
+        </label>
+      </li>
+      <br>
+      <li>
+        <label>
+          Password :
+          <input type="password" name="password">
+        </label>
+      </li>
+      <br>
+      <li>
+        <label>
+          <input type="checkbox" name="remember">
+          Remember Me
+        </label>
+      </li>
+      <br>
+      <li>
+        <button type="submit" name="login">Login</button>
+      </li>
+    </ul>
+  </form>
+
+</body>
+
+</html>
